c3bb82f8fbafd6e3d3522ec0a78cc890242b6c58
118355816 <118355816@BISB68VL.bis.ad.ucc.ie>
2019-03-21 09:14:16 +0000

    index

--- EBus.php
+++ EBus.php
@@ -1,79 +1,152 @@
+<?php
+session_start();
+$totalValue = "";
+$_SESSION['txtTotal'] = $totalValue;
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en" xmins="http.//www.w3.org/1999/xhtml">
     <head>
-        <title>Calculator</title>
-        <script language="javascript" type="text/javascript">
-         
-    function packageTotal() {
-        // Enter in prices here
-        var x = 5;
-        var y = 10;
-        var p = x + y * 12;
-        var b = y * 12;
-    
-    if (document.getElementById('basicProgram').checked) {
-        //Basic package is checked
-        document.calculator.total.value = b;
-    
-        
-     } else if (document.getElementById('proProgram').checked) {
-         //Basic package is checked 
-         document.calculator.total.value = p;
-     }
-     
-    }
-    
-    function calcDisc(){
-        var subTot = document.calculator.total.value;
-        var discTotal = ((subTot/ 100) *10);
-        document.calculator.discount.value=discTotal;
-    
-    }
-    function calcTotal(){
-        var subTott= document.calculator.total.value;
-        
-        var discTotal = document.calculator.discount.value;
-        
-        var wholeTotal = (subTott - discTotal);
-        
-        document.calculator.wholeTotal.value = wholeTotal;
-    }
-    </script>
+        <meta charset="utf-8" />
+        <title>Ebusiness 1</title>
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <style>
+            body{
+                background-color: rgb(277,210,111);
+                background-image: url(sky.jpg);
+            }
+            
+             .button {
+  background-color: blue;
+  border: none;
+  color: #070707;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  font-family: Times, serif;
+  font-weight: Bold;
+  
+            td {
+               
+                font-family: Times, serif;
+                color: rgb(37,38,39);
+                font-size: 20px
+                
+            }
+           
+        </style>
     </head>
-    <body style="background-image: url(sky.jpg);">
-       <!---Opening a HTML --->
-       <form name="calculator" method="post" action="Ebus2.php">
-                       <center>
-
-       <!---User fills out form here--->
-       <br/>
-       <input type="radio" name="programType" id="basicProgram" value="Basic"/>Basic
-       <input type="radio" name="programType" id="proProgram" value="Pro"checked/>Pro
-       
-       <!----Here result will be displayed. --->
-       <br />
-       subtotal: <input type="text" name="total">
+        <body>
+        <div class="form">
+            <form name="intCalc" method="post" action="Ebus2.php">
+        <h1 style="text-align:center;background-color: Blue;">Shop Calculator</h1>    
+        <hr/>
+        <center>
+            <table cellspacing="5">
+                <tr>
+                    <td style="color:black; font-size:50px; background-color: Blue;"><b>Consulting Service</b></td> 
+                </tr>
+                <tr>
+                    <td style="background-color: Blue;">BlockChain</td>
+                    <td><input type="radio" id="BlockChain" name="rdoGroup" value="1000" /></td>
+                </tr>
+                <tr>
+                    <td style="background-color: Blue;">Autonomous Things</td>
+                    <td><input type="radio" id="AutonomousThings" name="rdoGroup" value="2000" /></td>
+                </tr>
+                <tr>
+                    <td style="background-color: Blue;">Immersive Experience</td>
+                    <td><input type="radio" id="ImmersiveExperience" name="rdoGroup" value="3000" /></td>
+                </tr>
+            </table>
+        </center>
+        <br/>
+            <center>
+                 <table cellspacing="5">
+            <tr>
+                    <td><b></b></td>
+                    <td style="background-color: Blue;"><b>Amount</b></td>
+            </tr>
+                <tr>
+                    <td style="background-color: Blue;">Sub Total</td>
+                    <td><input type="text" id="txtSubTot" name="txtSub" readonly /></td>
+                </tr>
+                <tr>
+                    <td style="background-color: Blue;">Discount @ 10%</td>
+                    <td><input type="text" id="txtDisc" name="txtDisc" readonly /></td>
+                </tr>
+                <tr>
+                    <td style="background-color: Blue;">+ VAT @ 20%</td>
+                    <td><input type="text" id="txtVAT" name="txtVAT" readonly /></td>
+                </tr>
+                <tr>
+                    <td style="background-color: Blue;">Total</td>
+                    <td><input type="text" id="txtTotal" name="txtTotal" value="" readonly/></td>
+                </tr>
+            </table>
+        </center>
+        
+        <br />
+            <center>
+            <input class="button" type="button" name="btnCalc" id="btnCalc" onclick="calcSub()" value="Calculate Amount"/>
+            <input class="button" type="button" name="btnClear" id="btnClear" onclick="AmountClear()" value="Clear"/>
+            <input class="button" type="submit" name="btnProceed" id="btnProceed" onclick="" value="Proceed"/>
+        </center>
+            </form>
+        </div>
+        
+            <script type="text/javascript">
+                function calcSub(){
+                    //Assigning variables to the values
+                    var subAmount = parseFloat(document.getElementById('txtSubTot').value);
+                    var BlockChain = parseFloat(document.getElementById('BlockChain').value);
+                    var AutonomousThings = parseFloat(document.getElementById('AutonomousThings').value);
+                    var ImmersiveExperience = parseFloat(document.getElementById('ImmersiveExperience').value);
+                    
+                    // if radio buttons are clicked, values are assigned
+                    if (document.getElementById('BlockChain').checked) {
+                        document.intCalc.txtSubTot.value = BlockChain;
+                        subAmount = BlockChain;
+                        calcDisVatTotal(subAmount);
                         
-                        <br>
-       <input type="button" value="Subtotal" onclick="javascript:packageTotal();">
-       <br/>
-       
-       Discount: <input type="text" name="discount">
-       <br/>
-       <input type="button" value="Discount" onclick="javascript:calcDisc();">
-       <br/>
-       Total: <input type="text" name="wholeTotal">
-       <br>
-       <input type="button" value="Total" onclick="javascript:calcTotal();">
-       
-      <center>   
-                <input style="background-color: blue;" class="button" type="submit" name="btnContinue" id="btnContinue" onclick="" value="Continue"/>
-       
-       </form>
+                    }
+                   else if (document.getElementById('AutonomousThings').checked) {
+                        document.intCalc.txtSubTot.value = AutonomousThings;
+                        subAmount = AutonomousThings;
+                        calcDisVatTotal(subAmount);
+                        
+                    }
+                    else if (document.getElementById('ImmersiveExperience').checked) {
+                        document.intCalc.txtSubTot.value = ImmersiveExperience;
+                        subAmount = ImmersiveExperience;
+                        calcDisVatTotal(subAmount);
+                    }
+                } 
+                
+                //function for calculationg the values//
+                function calcDisVatTotal(parmSubTotal) {
+                    var subTotal = parseFloat(parmSubTotal);
+                    var discCalc = parseFloat(subTotal * .10);
+                    var vatCalc= parseFloat(subTotal * .20);    
+                    var total = parseFloat(subTotal - discCalc + vatCalc);
+                    
+                    //Inserting them into the correct fields//
+                    document.intCalc.txtDisc.value = discCalc;
+                    document.intCalc.txtVAT.value = vatCalc;
+                    document.intCalc.txtTotal.value = total;
+                }
+                
+                function AmountClear() {
+                    document.getElementById("txtSubTot").value="";
+                    document.getElementById("txtDisc").value="";
+                    document.getElementById("txtVAT").value="";
+                    document.getElementById("txtTotal").value="";
+                }
+                    
+            </script>
     </body>
-</html>
+  
+    </html>
+
+
