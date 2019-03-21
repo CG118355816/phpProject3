b6138d0383a8357a8b31ee38bafbb40853e77b37
118355816 <118355816@BISB68VL.bis.ad.ucc.ie>
2019-03-21 09:33:31 +0000

    index

--- index.php
+++ index.php
@@ -6,11 +6,10 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Granny's Consulting Service</title>
+        <title>Homepage</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-   <style>
+            <style>
 * {
   box-sizing: border-box;
 }
@@ -20,54 +19,54 @@ body {
   margin: 0;
 }
 
-/* Style the header */
+
 .header {
   padding: 20px;
   text-align: center;
-  background: yellow;
-  color: black;
+  background: #1abc9c;
+  color: white;
 }
 
-/* Increase the font size of the h1 element */
+
 .header h1 {
   font-size: 40px;
 }
 
-/* Style the top navigation bar */
+
 .navbar {
   overflow: hidden;
   background-color: #333;
 }
 
-/* Style the navigation bar links */
+
 .navbar a {
   float: left;
   display: block;
   color: white;
   text-align: center;
-  padding: 20px 210px;
+  padding: 20px 200px;
   text-decoration: none;
 }
 
-/* Right-aligned link */
+
 .navbar a.right {
   float: right;
 }
 
-/* Change color on hover */
+
 .navbar a:hover {
   background-color: #ddd;
   color: black;
 }
 
-/* Column container */
+
 .row {  
   display: flex;
   flex-wrap: wrap;
 }
 
-/* Create two unequal columns that sits next to each other */
-/* Sidebar/left column */
+
+
 .side {
   flex: 30%;
   background-color: #f1f1f1;
@@ -77,25 +76,25 @@ body {
 /* Main column */
 .main {   
   flex: 70%;
-  background-color:pink;
+  background-color: blueviolet;
   padding: 20px;
 }
 
-/* Fake image, just for this example */
+
 .fakeimg {
   background-color: #aaa;
   width: 100%;
   padding: 20px;
 }
 
-/* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
+
 @media screen and (max-width: 700px) {
   .row {   
     flex-direction: column;
   }
 }
 
-/* Responsive layout - when the screen is less than 400px wide, make the navigation links stack on top of each other instead of next to each other */
+
 @media screen and (max-width: 400px) {
   .navbar a {
     float: none;
@@ -103,32 +102,28 @@ body {
   }
 }
 </style>
-    
-    
-    </head>
-    
-    
-    
-    
+</head>
+
     <body>
-        
+       
         <div class="header">
-  <h1>Granny's Consulting Services</h1>
-  <p>Est.2018</p> </div> 
+  <h1>My Webpage</h1>
+  <p> Colm Greany </p>
+   </div> 
 
   <div class="navbar">
-  <a href="About.html">About</a>
-  <a href="contact.html">Contact</a>
-  <a href="EBus.php">Services</a>
+  <a href="CV.html">My CV</a>
+  <a href="interests.html">Interests</a>
+  <a href="consultingservices.html">Services</a>
   </div>
-  
-  <div class="main">
-      <center>
-      <h1 align="center">Because Granny's Advice Is Always Best</h1> 
-    <img src="granny.jpg" style="width:1100px;height:600px;" >
-   
-</div>
         
-        
+        <div class="main">
+            <center>
+           <h1 align="center">Welcome</h1> 
+           <h2><a style="color: yellow;"  href="https://github.com/CG118355816/phpProject3">My Github</a></h2>
+           <h2><a  style="color: yellow;" href="https://cg118355816.herokuapp.com/">My Heroku</a></h2>
+     
+     <img src="agenda.jpg" alt="meeting" style="width:1375px;height:600px;">
+         </div>
     </body>
 </html>
